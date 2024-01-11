9af1fe639e693d3b6e2ee583b9ddb3c3517e065f
Zubeyr Bahadır Damar <zubeyr.damar@sanctionscanner.com>
2024-01-11 19:50:51 +0300

    Hangfire Installation

--- Hangfire.Web/Hangfire.Web/Startup.cs
+++ Hangfire.Web/Hangfire.Web/Startup.cs
@@ -20,13 +20,25 @@ namespace Hangfire.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            /*
+            |
+            |   Handle Hangfire Configurations   
+            |
+            */
+
+            services.AddHangfire(config =>
+            {
+                config.UseSqlServerStorage(Configuration.GetConnectionString("HangfireConnection"));
+            });
+
+            services.AddHangfireServer();
+
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,16 +48,22 @@ namespace Hangfire.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            /*
+            |
+            |   Set hangfire dashboard
+            |   www.your-website.com/hangfire
+            |
+            */
+
+            app.UseHangfireDashboard("/hangfire");
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
