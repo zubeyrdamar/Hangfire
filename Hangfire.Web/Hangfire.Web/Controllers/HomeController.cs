acd3e08fe26b6341615c5a99ef2dd7109c18b43b
Zubeyr Bahadır Damar <zubeyr.damar@sanctionscanner.com>
2024-01-13 15:46:36 +0300

    Delayed Job

--- Hangfire.Web/Hangfire.Web/Controllers/HomeController.cs
+++ Hangfire.Web/Hangfire.Web/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using Hangfire.Web.BackgroundJobs;
 using Hangfire.Web.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Hangfire.Web.Controllers
 {
@@ -40,6 +44,38 @@ namespace Hangfire.Web.Controllers
             */
 
             FireAndForget.Job_SendEmail("1", "Welcome to my application!");
+
+            return View();
+        }
+    
+        public IActionResult ImageSave()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ImageSave(IFormFile image)
+        {
+            if (image != null)
+            {
+                // init filename & path to save
+                string filename = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", filename);
+
+                // copy image to path
+                using(var stream = new FileStream(path, FileMode.Create))
+                {
+                    await image.CopyToAsync(stream);
+                }
+
+                /*
+                |
+                |   Delayed job is called here
+                |
+                */
+
+                string jobId = Jobs.Delayed.Job_AddWatermark(filename, "your message");
+            }
 
             return View();
         }
---
+++ Hangfire.Web/Hangfire.Web/Jobs/Delayed.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace Hangfire.Web.Jobs
+{
+    public class Delayed
+    {
+        public static string Job_AddWatermark(string filename, string watermark)
+        {
+            return Hangfire.BackgroundJob.Schedule(() => ApplyWatermark(filename, watermark), TimeSpan.FromSeconds(10));
+        }
+
+        public static void ApplyWatermark(string filename, string watermarkText)
+        {
+            // set file path
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", filename);
+
+            // initializing a new image (holds image)
+            using (var bitmap = Bitmap.FromFile(path))
+            {
+                // initializing a new image frame (holds empty frame with image's height and width)
+                using(Bitmap tempBitmap = new Bitmap(bitmap.Width, bitmap.Height))
+                {
+                    // add text on image
+                    using(Graphics graphics = Graphics.FromImage(tempBitmap))
+                    {
+                        graphics.DrawImage(bitmap, 0, 0);
+                        var font = new Font(FontFamily.GenericSansSerif, 10, FontStyle.Bold);
+                        var color = Color.FromArgb(255, 0, 0);
+                        var brush = new SolidBrush(color);
+                        var point = new Point(20, bitmap.Height - 50);
+                        graphics.DrawString(watermarkText, font, brush, point);
+
+                        tempBitmap.Save(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/watermarks", filename));
+                    }
+                }
+            }
+        }
+    }
+}
