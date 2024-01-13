edcea1ca16124e2c908005c59898557a82e76861
Zubeyr Bahadır Damar <zubeyr.damar@sanctionscanner.com>
2024-01-13 19:07:28 +0300

    Recurring Task

---
+++ Hangfire.Web/Hangfire.Web/Jobs/Recurring.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Hangfire.Web.Jobs
+{
+    public class Recurring
+    {
+        public static void Job_Recurring()
+        {
+            Hangfire.RecurringJob.AddOrUpdate("reportJob_1", () => RecurringMethod(), Cron.Minutely);
+        }
+
+        public static void RecurringMethod()
+        {
+            Debug.WriteLine("This task is running periodically. Reported at: " + DateTime.Now.ToString());
+        }
+    }
+}
